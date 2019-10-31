4a91e81db3294c3308933b9403814502631fe715
fabiocuri <fcuri91@gmail.com>
2019-10-31 10:18:08 +0000

    Update impact.sh

--- impact.sh
+++ impact.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 folder=/home/bsclife018/Desktop/socialimpact
-#shopt -s globstar
-#echo "Converting pdfs to txt files..."
-#for file in ${folder}/data/**/**/*.pdf
-#do
-#  mv "$file" "${file// /_}"  
-#  echo ${file}
-#  python2 /home/bsclife018/Desktop/socialimpact/pdfminer-master/pdf2txt.py ${file// /_} > ${file// /_}.txt
-#done
-#echo "Finished converting!"
+shopt -s globstar
+echo "Converting pdfs to txt files..."
+for file in ${folder}/data/**/**/*.pdf
+do
+  mv "$file" "${file// /_}"  
+  echo ${file}
+  python2 /home/bsclife018/Desktop/socialimpact/pdfminer-master/pdf2txt.py ${file// /_} > ${file// /_}.txt
+done
+echo "Finished converting!"
 python3 ${folder}/inspect_reports.py
 python3 ${folder}/retrieve_tweets.py
